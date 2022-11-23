defb477c65e54a38bde67b3f95348c4dbd8d0358
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-11-23 12:44:31 +0530

    some updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -320,3 +320,63 @@ $(document).ready(function(){
 })()
 
 
+    function myFunctionOne() {
+      var dots = document.getElementById("dots");
+      var moreText = document.getElementById("more");
+      var btnText = document.getElementById("myBtn");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+    function myFunctionTwo() {
+      var dots = document.getElementById("dotsTwo");
+      var moreText = document.getElementById("moreTwo");
+      var btnText = document.getElementById("myBtnTwo");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+    function myFunctionThree() {
+      var dots = document.getElementById("dotsThree");
+      var moreText = document.getElementById("moreThree");
+      var btnText = document.getElementById("myBtnThree");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+    function myFunctionFour() {
+      var dots = document.getElementById("dotsFour");
+      var moreText = document.getElementById("moreFour");
+      var btnText = document.getElementById("myBtnFour");
+    
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
